c8d408b3d4528f6d49b46d9609c74029d5bae977
Iamneo <techadmin@iamneo.ai>
2023-11-26 14:25:58 +0000

    Backup Commit

--- angularapp/src/models/job-position.model.ts
+++ angularapp/src/models/job-position.model.ts
@@ -3,11 +3,11 @@ import { JobApplication } from "./job-application.model"
 export interface JobPosition {
     id:number
     title:string
-    department:string
-    location:string
-    responsibilities:string
-    qualifications:string
-    isClosed:boolean;
-    applicationDeadline:Date;
+    department?:string
+    location?:string
+    responsibilities?:string
+    qualifications?:string
+    isClosed?:boolean;
+    applicationDeadline?:Date;
     // applications:JobApplication[];
 }

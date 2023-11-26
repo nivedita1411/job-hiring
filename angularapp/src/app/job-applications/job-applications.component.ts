d971547a1f83619123d8d750521023f49b48c37d
Iamneo <techadmin@iamneo.ai>
2023-11-26 12:46:07 +0000

    Backup Commit

--- angularapp/src/app/job-applications/job-applications.component.ts
+++ angularapp/src/app/job-applications/job-applications.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JobService } from '../services/job.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { JobPosition } from 'src/models/job-position.model';
 
 @Component({
   selector: 'app-job-applications',
@@ -12,25 +13,36 @@ export class JobApplicationsComponent implements OnInit {
   constructor(private jb:JobService, private fb:FormBuilder) { }
 
   jobApplicationData : any
+  jobPositions : JobPosition[] = []
 
   jobApplicationForm = this.fb.group({
     jobPositionId:['', Validators.required],
     applicantName : ['',Validators.required],
-    status : ['pending']
+    // status : ['pending']
   })
 
   applyForJob()
   {
-    this.jobApplicationData = this.jobApplicationForm.value
-    console.log(this.jobApplicationData)
-    this.jb.applyForJob(this.jobApplicationData).subscribe(
-      ()=> {
-        
-      }
-    )
+
+    if(this.jobApplicationData.valid)
+    {
+      this.jobApplicationData = this.jobApplicationForm.value
+      console.log(this.jobApplicationData)
+      this.jb.applyForJob(this.jobApplicationData).subscribe(
+        ()=> {
+
+        }
+      )
+    }
+    
   }
 
   ngOnInit(): void {
+
+    this.jb.getJobPostings().subscribe((data:JobPosition[]) => {
+      this.jobPositions.push(...data)
+      console.log(this.jobPositions)
+    })
   }
 
 }
